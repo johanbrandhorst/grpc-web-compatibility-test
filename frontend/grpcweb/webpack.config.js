99d1ba87209cf055b17eca19aefcfa691a334549
Johan Brandhorst <johan.brandhorst@gmail.com>
2018-10-22 19:46:16 +0100

    Experiment with grpcweb client

--- frontend/grpcweb/webpack.config.js
+++ frontend/grpcweb/webpack.config.js
@@ -1,4 +1,25 @@
+const webpack = require("webpack");
+const path = require("path");
+
 module.exports = {
-    mode: 'production',
-    entry: './dist/client.js',
+  entry: "./src/index.ts",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "html", "build"),
+    filename: "bundle.js"
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: /src|proto/,
+        exclude: /node_modules/,
+        loader: "ts-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  }
 };
