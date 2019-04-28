703d9b17974b273640778184950320c641b95f69
Alex Stapleton <alexs@prol.etari.at>
2019-04-28 14:49:04 +0100

    Set magic Chrome params

--- frontend/improbable/test/test.ts
+++ frontend/improbable/test/test.ts
@@ -6,18 +6,27 @@ import { Runner, Suite } from 'mocha';
 var browser: puppeteer.Browser;
 
 before(async () => {
-    browser = await puppeteer.launch();
-})
+    browser = await puppeteer.launch({
+        args: [
+            // Required for Docker version of Puppeteer
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            // This will write shared memory files into /tmp instead of /dev/shm,
+            // because Docker’s default for /dev/shm is 64MB
+            '--disable-dev-shm-usage'
+        ]
+    });
+});
 
 after(() => {
     browser.close();
 });
 
-describe('serve static', function () {
+describe('client test', function () {
     var server = httpServer.createServer({ root: '.' });
     server.listen(8080);
 
-    it('callback', function (done) {
+    it('in browser', function (done) {
         (async () => {
             const page = await browser.newPage();
             await page.goto('http://localhost:8080/html/index.html');
