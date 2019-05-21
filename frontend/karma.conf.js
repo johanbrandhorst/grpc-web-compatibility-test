fe6789b91a13582c112d24145d76cc79faf75f5a
Alex Stapleton <alex.stapleton@ly.st>
2019-05-21 15:46:59 +0100

    Run all the tests by default

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function (config) {
     },
 
     // list of files / patterns to load in the browser
-    files: ['improbable/*Spec.ts'],
+    files: ['**/*Spec.ts'],
 
     // list of files / patterns to exclude
     exclude: [
