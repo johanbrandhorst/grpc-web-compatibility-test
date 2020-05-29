14729d9176da219fec4c375d0388b1e68e66e104
Johan Brandhorst <johan.brandhorst@gmail.com>
2020-05-29 17:21:07 +0100

    Update grpcwsgi proxy to sonora

--- .circleci/start-grpcwsgi.sh
+++ .circleci/start-grpcwsgi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo pip3 install grpcio protobuf grpcWSGI
+sudo pip3 install grpcio protobuf sonora
 
 python3 ./proxy/grpcwsgi/server.py
--- proxy/grpcwsgi/server.py
+++ proxy/grpcwsgi/server.py
@@ -1,18 +1,17 @@
 import sys
 from wsgiref.simple_server import make_server
-
-import grpc
-
-import grpcWSGI.server
+import sonora.wsgi
 
 import echo_pb2
 import echo_pb2_grpc
+
+import grpc
 
 
 class Echo(echo_pb2_grpc.EchoServiceServicer):
     def Echo(self, request, context):
         return echo_pb2.EchoResponse(message=request.message)
-    
+
     def EchoAbort(self, request, context):
         context.set_code(grpc.StatusCode.ABORTED)
         return echo_pb2.EchoResponse(message=request.message)
@@ -28,7 +27,7 @@ class Echo(echo_pb2_grpc.EchoServiceServicer):
 
 
 def main(args):
-    grpc_wsgi_app = grpcWSGI.server.grpcWSGI(None)
+    grpc_wsgi_app = sonora.wsgi.grpcWSGI(None)
 
     with make_server("", 8080, grpc_wsgi_app) as httpd:
         echo_pb2_grpc.add_EchoServiceServicer_to_server(Echo(), grpc_wsgi_app)
